42ef1661298eb0b649894eb6c730404ea9b68274
S-shariffa <syeadashariffa@gmail.com>
2025-02-20 15:42:40 +0530

    all

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,19 +7,46 @@ const Home = () => {
      <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
   <div class="col">
     <div class="card">
-      <img src="/ban.jpg" class="card-img-top" alt="..."/>
+      <img src="/briyani.jpg" class="card-img-top w=150" alt="..."/>
       <div class="card-body">
-        <h5 class="card-title">Veg Recipes</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Briyani</h5>
+        <p class="card-text">For a restaurant-style mutton biryani, you will need the following ingredients and steps:
+
+Ingredients:
+
+500g Basmati rice
+750g mutton, cut into pieces
+4 large onions, thinly sliced
+2 tomatoes, chopped
+1 cup plain yogurt
+4-5 green chilies, slit
+2-3 tbsp ginger-garlic paste
+1/2 cup chopped mint leaves
+1/2 cup chopped coriander leaves
+Whole spices: 4-5 cloves, 4-5 green cardamom, 1-2 cinnamon sticks, and 1 bay leaf
+4 cups water
+3-4 tbsp ghee or cooking oil
+Salt, to taste
+Saffron strands (optional)
+2-3 tbsp milk (optional, for saffron)</p>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="card">
-      <img src="/ban.jpg" class="card-img-top" alt="..."/ >
+      <img src="/chicken tadoori.png" class="card-img-top width=150" alt="..."/ >
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Chicken Tandoori</h5>
+        <p class="card-text">½ cup Greek yogurt (hung curd)
+1 tablespoon ginger garlic paste (or use ¾ tbsp each grated)
+1 teaspoon garam masala (more if you prefer)
+1 to 2 teaspoon Kashmiri red chili powder (or use paprika or a smoked hot chili powder, use 1 tsp for low heat and 2 tsps for high heat)
+¼ teaspoon black pepper crushed (powder)
+1½ teaspoon coriander powder (daniya powder)
+½ teaspoon cumin powder
+½ teaspoon sea salt (or use ¼ teaspoon each sea salt & black salt/ kala namak)
+¼ teaspoon turmeric (haldi)
+1½ teaspoon kasuri methi (dried fenugreek leaves)</p>
       </div>
     </div>
   </div>
